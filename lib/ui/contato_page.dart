4392a523f39cdb4bd74431b218cabfb2fe55ec98
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-06-03 15:05:25 -0300

    Editando a foto do contato, excluindo e fazendo ligacoes

--- lib/ui/contato_page.dart
+++ lib/ui/contato_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:contatos/helps/contato_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContatoPage extends StatefulWidget {
   final Contato contato;
@@ -18,7 +22,8 @@ class _ContatoPageState extends State<ContatoPage> {
 
   final _nomeController = TextEditingController();
   final _emailController = TextEditingController();
-  final _telefoneControler = TextEditingController();
+  final _telefoneController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
@@ -31,24 +36,31 @@ class _ContatoPageState extends State<ContatoPage> {
       contatoEditar = Contato.from(widget.contato.toMap());
       _nomeController.text = contatoEditar.nome;
       _emailController.text = contatoEditar.email;
-      _telefoneControler.text = contatoEditar.telefone;
+      _telefoneController.text = contatoEditar.telefone;
 
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+
         appBar: AppBar(
           title: Text(contatoEditar.nome ?? 'Novo Contato'),
           backgroundColor: Colors.red,
           centerTitle: true,
         ),
-        backgroundColor: Colors.white,
+
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             if(contatoEditar.nome != null && contatoEditar.nome.isNotEmpty){
               Navigator.pop(context, contatoEditar);
+
             }
             else{
               FocusScope.of(context).requestFocus(_nomeFocus);
@@ -57,7 +69,120 @@ class _ContatoPageState extends State<ContatoPage> {
           child: Icon(Icons.save),
           backgroundColor: Colors.red,
         ),
-      body: SingleChildScrollView()
+
+
+        body: SingleChildScrollView(
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: contatoEditar.imagem != null
+                              ? FileImage(File(contatoEditar.imagem))
+                              : AssetImage('images/person.png')
+                      )
+                  ),
+                ),
+                  onTap: (){
+                    _picker.getImage(source: ImageSource.camera).then((file) {
+                      if(file==null) return;
+                      setState(() {
+                        contatoEditar.imagem = file.path;
+                      });
+                    });
+                  }
+                ),
+
+                TextField(
+                  controller: _nomeController,
+                  focusNode: _nomeFocus,
+                  decoration: InputDecoration(
+                      labelText: "Nome",
+                      labelStyle: TextStyle(color: Colors.red),
+                      border: OutlineInputBorder()
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  onChanged: (text){
+                    _userEdit = true;
+                    setState((){
+                      contatoEditar.nome = text;
+                    });
+                  },
+                ),
+
+
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      labelText: "E-mail",
+                      labelStyle: TextStyle(color: Colors.red),
+                      border: OutlineInputBorder()
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  onChanged: (text){
+                    _userEdit = true;
+                    contatoEditar.email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+
+
+                TextField(
+                  controller: _telefoneController,
+                  decoration: InputDecoration(
+                      labelText: "Telefone",
+                      labelStyle: TextStyle(color: Colors.red),
+                      border: OutlineInputBorder()
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  onChanged: (text){
+                    _userEdit = true;
+                    contatoEditar.telefone = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                )
+
+              ],
+            )
+        )
+    )
     );
   }
+
+  Future<bool> _requestPop(){
+    if(_userEdit){
+      showDialog(
+          context: context,
+          builder: (context){
+            return AlertDialog(
+              title: Text('Descartar as alterações?'),
+              content: Text('Se sair as alterações serão perdidas.'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancelar'),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                ),
+                TextButton(
+                  child: Text('Sim'),
+                  onPressed: (){
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  }),
+              ]
+            );
+      });
+      return Future.value(false);
+    }else{
+      return Future.value(true);
+    }
+  }
+
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:contatos/helps/contato_helper.dart';
 import 'package:contatos/ui/contato_page.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState(){
     super.initState();
-    getTodosContatos();
+    _getTodosContatos();
   }
 
   @override
@@ -103,7 +104,8 @@ class _HomePageState extends State<HomePage> {
             )),
       ),
       onTap: (){
-        _showContatoPage(contato: contatos[index]);
+        //_showContatoPage(contato: contatos[index]);
+        _showOptions(context, index);
       },
     );
   }
@@ -118,10 +120,83 @@ class _HomePageState extends State<HomePage> {
       else{
         helper.salvarContato(_recContato);
       }
+      _getTodosContatos();
     }
   }
 
-  void getTodosContatos(){
+  void _showOptions(BuildContext context, int index){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return BottomSheet(
+            onClosing: (){
+
+            },
+              builder: (context){
+                return Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextButton(
+                          child: Text('Editar',
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20
+                            ),
+                          ),
+                          onPressed: (){
+                            Navigator.pop(context);
+                            _showContatoPage(contato: contatos[index]);
+                          },
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextButton(
+                          child: Text('Ligar',
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20
+                            ),
+                          ),
+                          onPressed: (){
+                            launch("tel: ${contatos[index].telefone}");
+                          },
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextButton(
+                          child: Text('Excluir',
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20
+                            ),
+                          ),
+                          onPressed: (){
+                            helper.deleteContato(contatos[index].id);
+                            setState(() {
+                              contatos.removeAt(index);
+                              Navigator.pop(context);
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+          );
+        }
+    );
+  }
+
+  void _getTodosContatos(){
     helper.getContatos().then((value) {
       setState(() {
         contatos = value;
